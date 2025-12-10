6c549f8c762c2eb388cf2eab1861cdece9fcf16e
Ivo Nascimento <iannsp@gmail.com>
2025-12-10 09:41:50 -0300

    Improve Build
    Update the build script (/script/build.sh)
    
    * build.sh parameter to define the target.
    * use a function to centralize the build.
    * add messages to communicate failure/success.

--- service/scripts/build.sh
+++ service/scripts/build.sh
@@ -4,13 +4,36 @@ if [ ! -d "./bin" ]; then
     mkdir ./bin
 fi
 
-if [[ "$1" == "naiveCatalog" ]]; then
-    echo "Building ./bin/naivecatalog-cli..."
-    go build -o ./bin/naivecatalog-cli cmd/naivecatalog/main.go 
-    echo "Done!"
-    exit 0
-fi
+declare -A apps
 
-go build -o ./bin/service-naivecatalog cmd/naivecatalog/main.go 
-go build -o ./bin/service-webui cmd/webui/main.go 
-go build -o ./bin/service-whatsapp cmd/whatsapp/main.go 
+apps["naivecatalog"]="go build -o ./bin/naivecatalog-cli cmd/naivecatalog/main.go"
+
+build() {
+    target=$1
+    echo "Building $target."
+    eval "${apps["$target"]}"
+    success=$?
+    if [[ $success -eq 0 ]]; then
+        echo "Target $target successfully build."
+        exit 0
+    fi
+    exit 1
+}
+
+# check if build receive a parameter to define what to build.
+# if not, build all.
+# if build_target run ok
+if [[ $# -eq 1 ]]; then
+    target=$1
+    if [[ -v apps["$target"] ]]; then
+        build $target
+    fi
+    echo -e "Build Fail!\nTarget '$target' not found.\nSee the list:"
+    for target_to_build in "${!apps[@]}"; do
+        echo "- $target_to_build"
+    done
+    exit 1
+fi
+exit 0
+
+
